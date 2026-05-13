2909970db9d520a38700aab04cd6085b9650801b
AAMIRMMAHDI <156841411+AAMIRMMAHDI@users.noreply.github.com>
2026-05-13 21:41:08 +0330

    CLONE_10

--- app.py
+++ app.py
@@ -833,4 +833,10 @@ def queue_status(queue_id):
 
 # ====================== اجرا ======================
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    socketio.run(
+        app,
+        host='0.0.0.0',
+        port=5000,
+        debug=False,
+        allow_unsafe_werkzeug=True
+    )
